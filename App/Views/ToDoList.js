7b9f9404aa10981069b334ba1f25a40d21dafdac
kokikono <kk.k.0421@outlook.jp>
2017-05-21 22:02:21 +0900

    modified:バグ修正とDrawerを追加

--- App/Views/ToDoList.js
+++ App/Views/ToDoList.js
@@ -10,7 +10,8 @@ import {
 } from 'react-native';
 import {
   List,
-  ListItem
+  ListItem,
+  Button
 } from 'native-base'
 import {ListView} from 'realm/react-native';
 import ToDoStore from '../Stores/ToDoStore';
@@ -18,7 +19,11 @@ import Icon from 'react-native-vector-icons/Octicons';
 import GlobalStyle from '../Styles/GlobalStyle';
 import {NavigationActions} from 'react-navigation';
 import {SwipeListView,SwipeRow} from 'react-native-swipe-list-view';
+import Swipeout from 'react-native-swipeout';
 import * as ToDoAction from '../Actions/ToDoAction';
+import Drawer from 'react-native-drawer';
+import GroupList from './GroupList';
+
 export default class ToDoList extends React.Component{
   static navigationOptions = ({navigation})=>{
     const {navigate} = navigation;
@@ -36,130 +41,84 @@ export default class ToDoList extends React.Component{
         <TouchableOpacity
           style={GlobalStyle.leftView}
           onPress={()=>{
-              //navigate('Group');
+              navigate('GroupList');
           }}>
           <Text style={[GlobalStyle.linkView]}>Groups</Text>
         </TouchableOpacity>
       )
     }
   }
+
   constructor(props){
     super(props);
     this.state={
-      todos: new ListView.DataSource({rowHasChanged:(r1,r2)=>r1!==r2})
+      dataSource: new ListView.DataSource({rowHasChanged:(r1,r2)=>r1!==r2})
     }
   }
   componentWillMount(){
     ToDoStore.on('change',()=>{
       this.setState({
-        todos:this.state.todos.cloneWithRows(ToDoStore.getAll())
+        dataSource:this.state.dataSource.cloneWithRows(ToDoStore.getAll())
       });
-    })
+    });
     this.setState({
-      todos:this.state.todos.cloneWithRows(ToDoStore.getAll())
+      dataSource:this.state.dataSource.cloneWithRows(ToDoStore.getAll())
     });
   }
   render(){
     return(
-      <View>
-        <ListView
-          dataSource={this.state.todos}
-          renderRow={this.renderRow}/>
-      </View>
+      <Drawer
+        ref={c => this.drawer = c}
+        type="overlay"
+        tapToClose={true}
+        panCloseMask={0.5}
+        styles={drawerStyles}
+        openDrawerOffset={0.3}
+        panOpenMask={0.5}
+        side='right'
+        content={<GroupList/>}
+        >
+        <ToDoListView
+          dataSource={this.state.dataSource}/>
+      </Drawer>
+    );
+  }
+}
+class ToDoListView extends React.Component{
+  render(){
+    return(
+      <ListView
+        dataSource={this.props.dataSource}
+        renderRow={this.renderRow.bind(this)}/>
     );
   }
   renderRow(item,sectionIndex,rowIndex){
+    let swipeType = {
+      right:[
+        {
+          text:'削除',
+          type: 'delete',
+          onPress:()=>{
+            ToDoAction.deleteToDo(item)
+          }
+        }
+      ]
+    };
     return(
-      <View style={styles.standalone}>
-					<SwipeRow
-						leftOpenValue={75}
-						rightOpenValue={-150}
-					>
-						<View style={styles.standaloneRowBack}>
-            <Text>Left</Text>
-            <View style={[styles.backRightBtn, styles.backRightBtnLeft]}>
-                  <Text style={styles.backTextWhite}>Right</Text>
-                </View>
-                <TouchableOpacity style={[styles.backRightBtn, styles.backRightBtnRight]}
-                  onPress={()=>{
-                    ToDoAction.deleteToDo(item);
-                  }}>
-                  <Text style={styles.backTextWhite}>Delete</Text>
-                </TouchableOpacity>
-						</View>
-						<View style={styles.standaloneRowFront}>
-							<Text>{item.name}</Text>
-						</View>
-					</SwipeRow>
-				</View>
+      <Swipeout
+        right={swipeType.right}
+        rowID={rowIndex}
+        sectionIndex={sectionIndex}
+        autoClose={true}
+        >
+        <ListItem>
+          <Text>{item.name}</Text>
+        </ListItem>
+      </Swipeout>
     )
   }
 }
-const styles = StyleSheet.create({
-	container: {
-		backgroundColor: 'white',
-		flex: 1
-	},
-	standalone: {
-    borderBottomColor: '#CFCFCF',
-    borderBottomWidth: 1
-	},
-	standaloneRowFront: {
-		alignItems: 'center',
-		backgroundColor: '#FFFFFF',
-		justifyContent: 'center',
-		height: 50,
-	},
-	standaloneRowBack: {
-		alignItems: 'center',
-		backgroundColor: '#8BC645',
-		flex: 1,
-		flexDirection: 'row',
-		justifyContent: 'space-between',
-		padding: 15
-	},
-	backTextWhite: {
-		color: '#FFF'
-	},
-	rowFront: {
-		alignItems: 'center',
-		backgroundColor: '#CCC',
-		borderBottomColor: 'black',
-		borderBottomWidth: 1,
-		justifyContent: 'center',
-		height: 50,
-	},
-	rowBack: {
-		alignItems: 'center',
-		backgroundColor: '#DDD',
-		flex: 1,
-		flexDirection: 'row',
-		justifyContent: 'space-between',
-		paddingLeft: 15,
-	},
-	backRightBtn: {
-		alignItems: 'center',
-		bottom: 0,
-		justifyContent: 'center',
-		position: 'absolute',
-		top: 0,
-		width: 75
-	},
-	backRightBtnLeft: {
-		backgroundColor: 'blue',
-		right: 75
-	},
-	backRightBtnRight: {
-		backgroundColor: 'red',
-		right: 0
-	},
-	controls: {
-		alignItems: 'center',
-		marginBottom: 30
-	},
-	switchContainer: {
-		flexDirection: 'row',
-		justifyContent: 'center',
-		marginBottom: 5
-	},
-});
+const drawerStyles = {
+  drawer: { shadowColor: '#000000', shadowOpacity: 0.8, shadowRadius: 3},
+  main: {paddingLeft: 3},
+}
