f29a10fa7e30921209b7d7e0ee2c272545f5b81b
kokikono <kk.k.0421@outlook.jp>
2017-05-21 21:56:28 +0900

    add:GroupStore

--- App/Stores/GroupStore.js
+++ App/Stores/GroupStore.js
@@ -9,10 +9,47 @@ class GroupStore extends EventEmitter{
   }
   handelActions(action){
     switch (action.type) {
-      case 'CREATE':{
+      case 'CREATE_GROUP':
+        this.createGroup(action.item);
         break;
-      }
-      default:{}
+      case 'ADD_TODO':
+        this.addToDo(action.group,action.todo);
+        break;
     }
   }
+  getAll(){
+    return this.groups.sorted('id');
+  }
+  nextGroupId(){
+    let groups = this.groups.sorted('id',true);
+    if(groups.length==0){
+      return 1;
+    }
+    let maxval = groups[0];
+    return maxval.id + 1;
+  }
+  createGroup(item){
+    realm.write(()=>{
+      realm.create('Group',{
+        id:this.nextGroupId(),
+        name:item.name,
+        todos:null
+      })
+    });
+    this.emit('change');
+  }
+  findGroup(group){
+    return realm.objects('Group').filtered('id = '+group.id);
+  }
+  addToDo(group,todo){
+    let tarGroup=findGroup(group);
+    realm.write(()=>{
+      tarTodo.todos.append(todo);
+    });
+    this.emit('change');
+  }
 }
+
+const groupStore = new GroupStore;
+dispatcher.register(groupStore.handelActions.bind(groupStore));
+export default groupStore;
