e1b437a91b5bae52fcbbc23d8568914047c7831f
kokikono <kk.k.0421@outlook.jp>
2017-05-21 10:34:24 +0900

    削除機能登録

--- App/Actions/ToDoAction.js
+++ App/Actions/ToDoAction.js
@@ -3,3 +3,6 @@ import dispatcher from '../Dispatcher/dispatcher';
 export function addToDo(item){
   dispatcher.dispatch({type:'CREATE_TODO',item});
 }
+export function deleteToDo(item){
+  dispatcher.dispatch({type:'DELETE_TODO',item});
+}
