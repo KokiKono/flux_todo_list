a4bfa132ff22ee58f83c3244159a5c801e4fd55f
kokikono <kk.k.0421@outlook.jp>
2017-05-21 22:03:13 +0900

    Group関係のViewを登録

--- App/app.js
+++ App/app.js
@@ -2,10 +2,14 @@ import {AppRegistry} from 'react-native';
 import {StackNavigator} from 'react-navigation';
 import ToDoList from './Views/ToDoList';
 import ToDoInput from './Views/ToDoInput';
+import GroupList from './Views/GroupList';
+import GroupInput from './Views/GroupInput';
 
 const App = StackNavigator({
   ToDoList:{screen:ToDoList},
-  ToDoInput:{screen:ToDoInput}
+  ToDoInput:{screen:ToDoInput},
+  GroupList:{screen:GroupList},
+  GroupInput:{screen:GroupInput}
 });
 
 AppRegistry.registerComponent('FluxToDoList',()=>App);
