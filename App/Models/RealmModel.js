ba8d17070df7def59d7c692faf09dde20238b449
kokikono <kk.k.0421@outlook.jp>
2017-05-21 10:38:07 +0900

    ToDoオブジェクトにIDを追加、Groupオブジェクトを追加

--- App/Models/RealmModel.js
+++ App/Models/RealmModel.js
@@ -4,8 +4,29 @@ class ToDo extends Realm.Object{}
 ToDo.schema={
   name:'ToDo',
   properties:{
+    id:'int',
     name:'string'
   }
 };
+class Group extends Realm.Object{}
+Group.schema={
+  name:'Group',
+  properties:{
+    id:'int',
+    name:'string',
+    todos:{type:'list',objectType:'ToDo',default:null}
+  }
+};
 
-export default new Realm({schema:[ToDo]});
+export default new Realm({
+  schema:[ToDo,Group],
+  schemaVersion:1,
+  migration:function(oldRealm,newRealm){
+    //ToDoオブジェクトにidを追加
+    var oldToDo = oldRealm.objects('ToDo');
+    var newToDo = newRealm.objects('ToDo');
+    for(var index=0;index<oldToDo.length;index++){
+      newToDo[index].id = index+1;
+    }
+  }
+});
