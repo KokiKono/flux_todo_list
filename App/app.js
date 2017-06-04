a1b4eef64e7124261a1e3cf9376c1d03d97a258b
kokikono <kk.k.0421@outlook.jp>
2017-06-05 00:31:55 +0900

    modified:add navigate screen

--- App/app.js
+++ App/app.js
@@ -4,12 +4,20 @@ import ToDoList from './Views/ToDoList';
 import ToDoInput from './Views/ToDoInput';
 import GroupList from './Views/GroupList';
 import GroupInput from './Views/GroupInput';
+import ToDoEdit from './Views/ToDoEdit';
+import ToDoDetail from './Views/ToDoDetail';
 
 const App = StackNavigator({
   ToDoList:{screen:ToDoList},
   ToDoInput:{screen:ToDoInput},
   GroupList:{screen:GroupList},
-  GroupInput:{screen:GroupInput}
+  GroupInput:{screen:GroupInput},
+  ToDoEdit:{screen:ToDoEdit},
+  ToDoDetail:{screen:ToDoDetail}
+},
+{
+  mode:false,
+  isSave:false
 });
 
 AppRegistry.registerComponent('FluxToDoList',()=>App);
