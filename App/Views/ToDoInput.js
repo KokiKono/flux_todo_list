7cc03180ab3c1821783dfd1c1e361debae46537d
kokikono <kk.k.0421@outlook.jp>
2017-06-05 00:25:55 +0900

    modified:move save action component

--- App/Views/ToDoInput.js
+++ App/Views/ToDoInput.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {
-  Text
+  Text,
+  TouchableOpacity
 }from 'react-native';
 import {
   Container,
@@ -10,12 +11,28 @@ import {
   Button
 } from 'native-base';
 import {NavigationActions} from 'react-navigation';
+import GlobalStyle from '../Styles/GlobalStyle';
 import * as ToDoAction from '../Actions/ToDoAction';
 
 export default class ToDoInput extends React.Component{
   static navigationOptions = ({navigation}) => {
+    const {params} = navigation.state;
+    const link = params?params.saveMode?GlobalStyle.linkView:null:null;
     return{
-      title:'ToDoInput'
+      title:'ToDoInput',
+      headerRight:(
+        <TouchableOpacity onPress = {
+          () => {
+            let {params} = navigation.state;
+            params.saveFunction();
+            navigation.goBack();
+          }
+        }>
+          <Text style={[GlobalStyle.rightView,link]}>
+            Save
+          </Text>
+        </TouchableOpacity>
+      )
     }
   }
   constructor(props){
@@ -23,6 +40,9 @@ export default class ToDoInput extends React.Component{
     this.state={
       text:''
     }
+    props.navigation.setParams({
+      saveFunction:this.addToDo.bind(this)
+    });
   }
   render(){
     return(
@@ -30,16 +50,18 @@ export default class ToDoInput extends React.Component{
         <Content>
           <InputGroup regular>
             <Input placeholder='ToDo Name'
-              onChangeText={(text)=>this.setState({text})}
+              onChangeText={(text)=>this.onChangeText(text)}
               value={this.state.text}/>
           </InputGroup>
-          <Button full
-            onPress={()=>this.addToDo()}>
-            <Text>保存</Text>
-         </Button>
         </Content>
       </Container>
     )
+  }
+  onChangeText(text){
+    this.setState({text:text});
+    this.props.navigation.setParams({
+      saveMode:text.length==0?false:true
+    });
   }
   addToDo(){
     const item = {
