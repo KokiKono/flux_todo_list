b0d7773206398fe0a0e2b70fd2609747d61afd05
kokikono <kk.k.0421@outlook.jp>
2017-06-05 00:32:21 +0900

    various

--- android/app/src/main/java/com/fluxtodolist/MainActivity.java
+++ android/app/src/main/java/com/fluxtodolist/MainActivity.java
@@ -1,8 +1,10 @@
 package com.fluxtodolist;
 
 import com.facebook.react.ReactActivity;
+import com.imagepicker.permissions.OnImagePickerPermissionsCallback;
+import com.facebook.react.modules.core.PermissionListener;
 
-public class MainActivity extends ReactActivity {
+public class MainActivity extends ReactActivity implements OnImagePickerPermissionsCallback{
 
     /**
      * Returns the name of the main component registered from JavaScript.
@@ -11,5 +13,23 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "FluxToDoList";
+    }
+
+    private PermissionListener listener;
+
+    @Override
+    public void setPermissionListener(PermissionListener listener)
+    {
+        this.listener = listener;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults)
+    {
+        if (listener != null)
+        {
+            listener.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
