39bb8683a22875fb309d5625348e4fee0a3cd9ef
kokikono <kk.k.0421@outlook.jp>
2017-06-13 11:03:45 +0900

    modified:image option detail

--- App/Models/RealmModel.js
+++ App/Models/RealmModel.js
@@ -24,14 +24,25 @@ Image.schema={
   properties:{
     id:'int',
     title:'string',
-    source:{type:'data',optional:true},
-    uri:'string'
+    source:{type:'string',optional:true},
+    uri:'string',
+    origURL:{type:'string',default:'',optional:true},
+    path:{type:'string',default:'',optional:true},
+    data:{type:'data',optional:true},
+    fileName:{type:'string',default:'',optional:true},
+    fileSize:{type:'int',default:-1,optional:true},
+    width:{type:'int',default:-1,optional:true},
+    height:{type:'int',default:-1,optional:true},
+    isVertical:{type:'bool',default:false,optional:true},
+    latitude:{type:'double',default:0,optional:true},
+    longitude:{type:'double',default:0,optional:true},
+    timestamp:{type:'date',default:null,optional:true}
   }
 }
 
 export default new Realm({
   schema:[ToDo,Group,Image],
-  schemaVersion:1,
+  schemaVersion:10,
   migration:function(oldRealm,newRealm){
 
   }
